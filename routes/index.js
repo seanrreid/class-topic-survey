23724acb40889a2b55487a4edee13b4b013cb1e5
Sean R Reid <sean@torchcodelab.com>
2019-06-03 14:25:47 -0400

    Added topic status list as a partial

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,30 @@ const ClassDataModel = require('../models/classInfo');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  const topicData = await ClassDataModel.getAll();
+  const topicData = await ClassDataModel.getAllTopicData();
+  const topicStatusList = await ClassDataModel.getAllClassStatus();
 
   res.render('template', { 
     locals: {
       title: 'Class Topic Ranking',
-      topicData: topicData
+      topicData: topicData,
+      topicStatusList: topicStatusList
     },
     partials: {
       partial: 'partial-index'
     }
   });
 });
+
+router.post('/update', (req, res) => {
+  console.log(req.body);
+
+  for(let key in req.body) {
+    ClassDataModel.update(key, req.body[key]);
+  }
+
+  res.status(200).redirect('/');
+});
+
 
 module.exports = router;
