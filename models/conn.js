3d8e887b09e8080d515709eff2401ede38c3ed68
Sean R Reid <sean@torchcodelab.com>
2020-03-02 08:33:01 -0500

    Updated the database name to match new DB

--- models/conn.js
+++ models/conn.js
@@ -1,14 +1,13 @@
-const pgp = require('pg-promise') ({
-    query: e => {
-        console.log('QUERY:', e.query);
-    }
-})
+const pgp = require('pg-promise')({
+  query: e => {
+    console.log('QUERY:', e.query);
+  }
+});
 
 const options = {
-    host: 'localhost',
-    database: 'digital_crafts_class',
-    user: 'ranger'
-}
+  host: 'localhost',
+  database: 'digital_crafts_class_survey'
+};
 
 const db = pgp(options);
 
